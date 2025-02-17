23758c79675678f777cefe8756aef87f8a780c0a
Harry Peter Miles <164933240+harrypmdev@users.noreply.github.com>
2025-02-17 18:26:26 +0000

    fix: add allow cors credentials to settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -107,6 +107,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_CREDENTIALS = True
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN'),
